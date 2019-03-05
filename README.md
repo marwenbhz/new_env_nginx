7910df0fb62749f4cdcc326c7b8656f6375be4cf
root <root@ip-172-31-93-77.ec2.internal>
2019-03-05 15:06:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# install nginx.
+
+1* git clone this repo.
+2* ./install_nginx.sh
---
+++ install_nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install nginx
+
+sudo ufw app list
+sudo ufw allow 'Nginx HTTP'
+
+systemctl status nginx
+
+ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
+
+sudo apt-get install curl
+curl -4 icanhazip.com
